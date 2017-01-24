27a41f318a8a236263c5b16d90020d4202680191
BillLee李泽宇 <purillmit@gmail.com>
2017-01-23 20:13:26 -0800

    Task 1 completed. Task 2 (a) completed

---
+++ NBD_Task2_a.R
@@ -0,0 +1,112 @@
+#################################################################
+##  EE219 Network-Backup-Dataset.
+##  Task 2. Computing P-value with linear regression.
+##  
+##  Zeyu Li
+##  Jan 23, 2017
+##
+##  * Package "DAAG and "cvTools" are required.
+##  * Modifying the attribute names of the "data.csv" is also 
+##      required since R cannot recognize space.
+#################################################################
+library(DAAG)
+library(cvTools)
+
+# Load dataset
+dataset <- read.csv("data.csv")
+
+# Create 10-fold cross validation
+k <- 10
+folds <- cvFolds(nrow(dataset), K=k)
+
+train_formula <- train$Size_of_Backup ~ 
+  train$WeekNum + factor(train$Day_of_Week) + 
+  train$Hour_of_Day +factor(train$Work_Flow_ID) + 
+  factor(train$File_Name) + train$Backup_Time
+
+# Re-write the formula so that it doesn't encounter # of feature problems
+train_formula <- df$Size_of_Backup ~ 
+  df$WeekNum + factor(df$Day_of_Week) + 
+  df$Hour_of_Day +factor(df$Work_Flow_ID) + 
+  factor(df$File_Name) + df$Backup_Time
+
+# Initialize an empty rmse list
+rmse_list <- c()
+pvalue_sum <- rep(0, 39)
+
+# Vectors to record Fitted values, Fitted residuals, and Actual values
+fitted_value_list <- c()
+fitted_resid_list <- c()
+actual_value_list <- c()
+
+for(i in 1:k){
+  # Set training set and validation set
+  # train <- dataset[folds$subsets[folds$which != i], ] 
+  # validation <- dataset[folds$subsets[folds$which == i], ]
+  
+  # Fit training data into "df"
+  df <- dataset[folds$subsets[folds$which != i], ] 
+  newlm <- lm(data = df, formula = train_formula)
+  # Modify the "NA" in coefficients to 0.00
+  newlm$coefficients[is.na(newlm$coefficients)] <- 0.00000000
+  
+  # Compute the RSME, append to list "rmse_list"
+  smry_lm <- summary(newlm)
+  rsdl <- smry_lm$residuals
+  rmse <- sqrt(mean((rsdl^2)))
+  rmse_list <- c(rmse_list, rmse)
+  
+  # Take the P-Values
+  pvalue <- coef(smry_lm)[,4]
+  pvalue_sum <- pvalue + pvalue_sum
+  
+  # Get Prediction
+  df <- dataset[folds$subsets[folds$which == i], ]
+  newpred <- predict(newlm,  newdata=df)
+  # Get predicted residuals and actual values
+  pred_residuals <- newpred - df$Size_of_Backup
+  actual_value <- df$Size_of_Backup
+  
+  # Add data to Lists
+  fitted_value_list <- c(fitted_value_list, newpred)
+  fitted_resid_list <- c(fitted_resid_list, pred_residuals)
+  actual_value_list <- c(actual_value_list, actual_value) 
+}
+
+# Compute the mean p-value of different attributes
+pvalue_sum <- pvalue_sum / 10
+
+# Print results we need
+pvalue_sum
+rmse_list
+
+# Plotting the images and save
+png(filename = "predicted_residual_vs_actual_value.png")
+plot(fitted_resid_list, actual_value_list, 
+     main = "Predicted Residuals v.s. Actual Value",
+     xlab = "Predicted Residuals", ylab = "Actual Value")
+dev.off()
+
+png(filename = "predicted_value_vs_predicted_residuals.png")
+plot(fitted_value_list, fitted_resid_list, 
+     main="Predicted Value v.s. Predicted Residuals",
+     xlab = "Predicted Value", ylab = "Predicted Residuals")
+dev.off()
+
+##
+# From the distribution of the p-value we found that some attributes have 
+# small p-values which means it closely related to the target value. 
+# Hence we define another linear_model to see how it works under the same 
+# setting: cross-validation.
+##
+
+train_formula_2 <- dataset$Size_of_Backup ~ 
+                    factor(dataset$Day_of_Week) + dataset$Hour_of_Day +
+                    factor(dataset$Work_Flow_ID) + dataset$Backup_Time
+
+lm_2 <- lm(data = dataset, formula = train_formula_2)
+
+plot(lm_2)
+
+
+
--- n1_plot.py
+++ n1_plot.py
@@ -55,23 +55,77 @@ if __name__ == '__main__':
     data = calculate_num_day(data, 'Num of the day', 'Week #', 'Number of Weekday')
 
     # Group the data by 'Week #', 'Number of Weekday' and take the sum of "Size of Backup (GB)"
-    grouped_data = data.groupby(['Week #', 'Number of Weekday'])['Size of Backup (GB)'].sum()
+    grouped_data = data.groupby(['Week #', 'Work-Flow-ID', 'Number of Weekday'])['Size of Backup (GB)'].sum()
+    # print grouped_data
     listed_data = list(grouped_data)
-    sep_listed_data = [listed_data[x:x+21] for x in range(0, len(listed_data), 21)]
+    print grouped_data
+    # print listed_data
+    # print len(listed_data)
 
-    # Started to plot
-    x_days = range(1, 22)
-    fig = plt.figure(figsize=(30, 20))
-    fig_list = []
-    print len(sep_listed_data)
-    for i in range(0, len(sep_listed_data)):
-        fig_list.append(fig.add_subplot(2, 3, i+1))
-        fig_list[i].plot(x_days, sep_listed_data[i], '-')
-        print sep_listed_data[i]
-        fig_list[i].set_xticks(x_days)
-        fig_list[i].set_title("Data of {}-th Week".format(i))
-        fig_list[i].set_xlabel("# of Days")
-        fig_list[i].set_ylabel("Sum of Size of Backup (GB)")
-        fig_list[i].set_xlim([1, 21])
-        fig_list[i].set_ylim([5, 20])
+    # The listed data has len of 525
+    # 15 Weeks * 5 Workflows * 7 Days/Week
+    # sep_listed_data = [listed_data[x:x+21] for x in range(0, len(listed_data), 21)]
+
+    # Divided by 3 weeks, should be 5 period
+    by_three_weeks = [listed_data[i:i+105] for i in range(0, len(listed_data), 105)]
+
+    period_list = []
+    # Divide by weeks and workflows, should be 25 sets
+    for period in by_three_weeks:
+        # workflow list over weekdays
+        list_all_wf = []
+        for wf in range(0, 5):
+            list_wf = []
+            for week in range(0,3):
+                base = week*35 + wf*7
+                list_wf_week = period[base:base+7]
+                list_wf += list_wf_week
+            list_all_wf.append(list_wf)
+        period_list.append(list_all_wf)
+
+
+    # for i in range(0,len(period_list)):
+    #     for j in range(0,5):
+    #         print period_list[i][j]
+    #         print len(period_list[i][j])
+
+
+    # Start to plot
+    x_days = np.arange(1,22)
+    fig = plt.figure(figsize=(30,20))
+
+    for i in period_list:
+        for j in i:
+            print len(j)
+    for i in range(0,len(period_list)):
+        sub_fig = fig.add_subplot(2,3, i+1)
+        # for j in range(0,5):
+            # y_data = np.array(period_list[i][j])
+        # y_data = period_list[i][3]
+        sub_fig.plot(x_days, period_list[i][1], '-',
+                     x_days, period_list[i][2], '-',
+                     x_days, period_list[i][3], '-',
+                     x_days, period_list[i][0], '-',
+                     x_days, period_list[i][4], '-')
+        sub_fig.set_xticks(x_days)
+        sub_fig.set_title("Data of {}-th Week".format(i))
+        sub_fig.set_xlabel("# of Days")
+        sub_fig.set_ylabel("Sum of Size of Backup (GB)")
+        sub_fig.set_xlim([0.5, 21.5])
+        sub_fig.set_ylim([0, 13])
     fig.savefig('NBD_Task1.png')
+
+
+
+
+     # for i in range(0, len(sep_listed_data)):
+    #     fig_list.append(fig.add_subplot(2, 3, i+1))
+    #     fig_list[i].plot(x_days, sep_listed_data[i], '-')
+    #     print sep_listed_data[i]
+    #     fig_list[i].set_xticks(x_days)
+    #     fig_list[i].set_title("Data of {}-th Week".format(i))
+    #     fig_list[i].set_xlabel("# of Days")
+    #     fig_list[i].set_ylabel("Sum of Size of Backup (GB)")
+    #     fig_list[i].set_xlim([1, 21])
+    #     fig_list[i].set_ylim([5, 20])
+    # fig.savefig('NBD_Task1.png')
